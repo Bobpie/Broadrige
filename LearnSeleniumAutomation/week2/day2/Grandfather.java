144a60c733a82523c25b62a7dafc8847ca5913bc
Bobpie <56382470+Bobpie@users.noreply.github.com>
2021-10-16 15:44:09 +0530

    Delete LearnSeleniumAutomation directory

--- LearnSeleniumAutomation/week2/day2/Grandfather.java
+++
@@ -1,9 +0,0 @@
-package week2.day2;
-
-public class Grandfather {
-public int a = 5;
-public void anscestoralHouse() {
-	Object obj = "Balaji";
-	System.out.println("Old house");
-}
-}
--- LearnSeleniumAutomation/week3/day1/PartiallyBuildHouse.java
+++
@@ -1,9 +0,0 @@
-package week3.day1;
-
-public abstract class PartiallyBuildHouse {
-public void buildGroundFloor() {
-	
-}
-
-public abstract void buildFirstFloor();
-}
